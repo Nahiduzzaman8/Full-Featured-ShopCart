2d7e92906ede76951d272036e12f81b026ac1e93
Nahiduzzaman8 <nahiduzzamantalukder@gmail.com>
2026-01-09 15:09:33 +0600

    static file configuration done

--- shopcart/settings.py
+++ shopcart/settings.py
@@ -103,6 +103,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     'shopcart/static'
 ]
+STATIC_ROOT = BASE_DIR /'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
