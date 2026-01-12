1363676a864b731cbbd54c8a43aa0c78b3badfc8
Nahiduzzaman8 <nahiduzzamantalukder@gmail.com>
2026-01-13 04:53:41 +0600

    added custom user and custom user manager

--- shopcart/settings.py
+++ shopcart/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
     'category'
 ]
 
@@ -110,3 +111,4 @@ STATIC_ROOT = BASE_DIR /'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.Account'
