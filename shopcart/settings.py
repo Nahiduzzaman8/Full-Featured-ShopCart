65231133caf1226a4ef150d9e1c488c7fca26cd8
Nahiduzzaman8 <nahiduzzamantalukder@gmail.com>
2026-01-14 01:34:40 +0600

    added product app along with Product model

--- shopcart/settings.py
+++ shopcart/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    'category'
+    'category',
+    'store'
 ]
 
 MIDDLEWARE = [
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name','category','slug','price','images','stock','modified_date','created_date','is_available')
+    prepopulated_fields = {'slug':('product_name', )}
+
+
+
+
+    
+admin.site.register(Product, ProductAdmin)
