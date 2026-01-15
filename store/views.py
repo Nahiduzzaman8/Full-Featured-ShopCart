5d4fd248985799f4f60b53c280ce62a647aa199a
Nahiduzzaman8 <nahiduzzamantalukder@gmail.com>
2026-01-15 23:22:33 +0600

    store_home

--- store/views.py
+++ store/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def store_home (req):
-    return render(req, 'store/store.html')
+    products = Product.objects.all().filter(is_available=True)
+    no_of_products = products.count()
+    context = {
+        'products':products, 
+        'no_of_products':no_of_products
+    }
+    return render(req, 'store/store.html', context)
